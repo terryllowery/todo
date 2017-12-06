1ca42ff2bbe0b63adb7e9ae46ad09522acad977c
Unknown <terry@lowerytech.net>
2017-12-05 23:25:49 -0600

    rspec test for task status
    Created test to update task status to done

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe TasksController, type: :controller do
 		end
 	end
 
+	describe "tasks#update" do
+		it "should update a task in database to done" do
+
+			task = FactoryBot.create(:task, done: false)
+			put :update, id: task.id, task: {done: true}
+			expect(response).to have_http_status(:success)
+			task.reload
+			expect(task.done).to eq(true)
+		end
+	end
+
 end
