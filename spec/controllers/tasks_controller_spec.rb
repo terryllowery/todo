94cfaec9fc160154f38d7bbd56353f02753c038f
Unknown <terry@lowerytech.net>
2017-12-12 19:50:25 -0600

    Lesson 15: Fixed broken rspec test
    Rspec test for create new task was not sending the params correctly.

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe TasksController, type: :controller do
 
 	describe "tasks#create" do
 		it "should allow a new task to be created" do
-			post :create, params: {title: "Fix things"}
+			post :create, params:  {task: {title: "Fix things"}}
 			expect(response).to have_http_status(:success)
 			response_value = ActiveSupport::JSON.decode(@response.body)
 			expect(response_value['title']).to eq("Fix things")
